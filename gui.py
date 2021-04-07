cf912e9963f0cad8446b5a077cf45a857315417b
Fabian Zwolinski <Giovacho@tuta.io>
2021-04-07 23:34:42 +0200

    Add documentation of gui file

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ import threading
 
 ss = SmartStudent()
 
+# All gui texts in specified lang
 gui_texts = {}
 
 # Load translation
@@ -29,28 +30,51 @@ root.title("SmartStudent")
 root.geometry(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}") 
 root.resizable(False,False)
 
+# Timer variable to allow taking ss's no more than once every 2 sec
 ss_click_time = -2
+
+# Timer variable to allow saving config no more than once every 2 sec
 save_config_click_time = -2
 
+# Controls output update loop
+# Set to True, breaks infinite loop 
 exit_program = False
 
 # Change Main View
 def set_home_page():
+  """
+  Sets main view to Home page
+  """
   set_main_view(home_frame)
 
 def set_config_page():
+  """
+  Sets main view to Config page
+  """
   set_main_view(config_frame)
   clear_config_status()
   insert_config_values()
   display_profiles()
 
 def set_desc_page():
+  """
+  Sets main view to Description page
+  """
   set_main_view(desc_frame)
 
 def set_info_page():
+  """
+  Sets main view to Info page
+  """
   set_main_view(info_frame)
 
 def set_main_view(p):
+  """
+  Allows to set current main view
+  Hides other views and load "p" (from parameter) view
+  
+  :param p: view to be set
+  """
   for view in main_views:
     if view != p:
       view.pack_forget()
@@ -104,7 +128,7 @@ main_description_btn.pack(side=LEFT)
 main_info_btn.pack(side=LEFT)
 ########################################  
 
-# Main Views
+############## Main Views ##############
 home_frame = Frame(
   root, 
   width=SCREEN_WIDTH, 
@@ -132,24 +156,37 @@ info_frame = Frame(
   bg=theme['MAIN_BG']
 )
 
+# list of all available main views
 main_views = [home_frame, config_frame, desc_frame, info_frame]
-############
+########################################
 
 ############ Home Page ############
 
+# Text of button running and stopping program
 run_stop = StringVar()
 run_stop.set(f"{gui_texts['run_program']}!")
+# Tells if program is running or not
 running = False
 
 def take_screenshot():
+  """
+  Calls take screenshot method
+  """
+  # Prevent button spam by limiting up to once per 2 sec
   global ss_click_time
   if (time.perf_counter() - ss_click_time) > 2:
     ss_click_time = time.perf_counter()
     ss.take_test_screenshot()
 
 def run_program():
+  """
+  Run / Stop program button
+  """
   global running
   if not running:
+    # If not running, run it
+    
+    # Make all btns not clickable
     disable_buttons_on_running()
     run_stop.set(gui_texts['stop_program'])
     run_program_btn.configure(
@@ -157,7 +194,9 @@ def run_program():
       activebackground=theme['STOP_PROGRAM_BTN_BG'], 
       fg=theme['STOP_PROGRAM_BTN_FG']
     )
+    # Run main program loop
     ss.run()
+    # Extend output field
     output_text.place(
       rely=1+0.01, 
       relx=0.5, 
@@ -166,6 +205,7 @@ def run_program():
       anchor=S
     )
   else:
+    # If running, stop it
     enable_buttons_on_stop()
     run_stop.set(f"{gui_texts['run_program']}!")
     run_program_btn.configure(
@@ -173,7 +213,9 @@ def run_program():
       activebackground=theme['RUN_PROGRAM_BTN_BG'], 
       fg=theme['RUN_PROGRAM_BTN_FG']
     )
+    # Stop main program loop
     ss.stop_program()
+    # Shrink output field
     output_text.place(
       rely=1+0.01, 
       relx=0.5, 
@@ -181,15 +223,22 @@ def run_program():
       width=SCREEN_WIDTH+1, 
       anchor=S
     )
+  # Switch running status to opposite
   running = not running
 
 def enable_buttons_on_stop():
+  """
+  Makes buttons enabled (clickable)
+  """
   take_screenshot_btn['state'] = "normal"
   main_home_btn['state'] = "normal"
   main_config_btn['state'] = "normal"
   main_info_btn['state'] = "normal"
 
 def disable_buttons_on_running():
+  """
+  Makes buttons disabled (un-clickable)
+  """
   take_screenshot_btn['state'] = "disable"
   main_home_btn['state'] = "disable"
   main_config_btn['state'] = "disable"
@@ -228,24 +277,36 @@ output_text = Text(
 )
 output_text.place(rely=1+0.01 , relx=0.5, height=180, width=SCREEN_WIDTH+1, anchor=S)
 
+# Current length of output
 output_widget_len = 0
 
 def update_output():
+  """
+  Shows all outputs in output field
+  """
   global output_widget_len
   
   while not exit_program:
+    # get output, and output lenght from class object
     output, out_len = ss.get_output()
+    # set output field "insertable"
     output_text.config(state=NORMAL)
     
+    # Insert all the outputs that aren't inserted yet
+    # by comparing current output lenght with output from class obj
     for o in output[output_widget_len : out_len]:
       output_text.insert(END, "- " + o + "\n")
 
+    # Update current output lenght
     output_widget_len = out_len
+    # Scroll widget to the very bottom
     output_text.see("end")
+    # set output field "not-insertable"
     output_text.config(state=DISABLED)
     
     time.sleep(3)
 
+# Update output field in the background
 output_update = threading.Thread(target=update_output)
 output_update.setDaemon(True)
 output_update.start()
@@ -259,7 +320,7 @@ window_names = [x for x in windows.values()]
 langs = ss.get_available_translations()
 themes = ss.get_available_themes()
 
-# Inputs
+# Config inputs
 clicked_lang = StringVar()
 clicked_theme = StringVar()
 clicked_window = StringVar()
@@ -275,6 +336,9 @@ coords_validate = StringVar()
 save_file_success = StringVar()
 
 def insert_config_values():
+  """
+  Inserts all current config values to config view inputs
+  """
   # Language
   clicked_lang.set(ss.config["lang"])
 
@@ -304,6 +368,9 @@ def insert_config_values():
   coords_br.set(str(ss.config_profile.get("bottom_right_coords")))
 
 def clear_config_values():
+  """
+  Clear config inputs
+  """
   # Window ID
   clicked_window.set(window_names[0])
 
@@ -322,17 +389,31 @@ def clear_config_values():
   coords_br.set("")
 
 def set_path():
+  """
+  Prompt path set
+  """
   path_dir = filedialog.askdirectory()
   path.set(path_dir)
 
 def save_config(action):
+  """
+  Saves / Updates config
+  Creates new / Renames profile
+  
+  :return: False if config values are incorrect (config not validated)
+  """
+
+  # Prevent button click spam
+  # Max once per 2 sec
   global save_config_click_time
   if (time.perf_counter() - save_config_click_time) < 2:
     return
   save_config_click_time = time.perf_counter()
 
+  # Check if config values are correct
   if not validate_config():
     return False
+  # New config dict (also may be dict to update config)
   new_config = {
     "window_id": int(list(windows.keys())[list(windows.values()).index(clicked_window.get())]),
     "step": int(config_step_entry.get()),
@@ -346,22 +427,34 @@ def save_config(action):
   # Save config/profile (Update)
   if action == "save":
     if ss.update_config_profile(ss.config['current_profile'], new_config):
+      # Profile updated, display success msg
       save_file_success.set(f"{gui_texts['saved']}!")
     else:
+      # Profile not updated, display error msg
       save_file_success.set(f"{gui_texts['error']}!")
   # Create new profile
   elif action == "add":
     if ss.create_new_profile(profile_name.get(), new_config):
+      # Update profiles list
       display_profiles()
   # Rename profile
   elif action == "rename":
     if ss.rename_profile(ss.config['current_profile'], profile_name.get()):
       hide_profile_name_input()
+      # Update profiles list
       display_profiles()
 
 def validate_config():
+  """
+  Verifies config for correct values
+
+  :return:
+    True if config values are correct
+    False if config values are incorrect
+  """
   correct_values = True
   try:
+    # Step must be int and be > 0
     step = int(config_step_entry.get())
     if step > 0:
       step_validate.set(gui_texts['good'])
@@ -373,6 +466,7 @@ def validate_config():
     correct_values = False
 
   try:
+    # Percentage difference must be float and be in range [0; 100]
     diff_perc = float(config_diff_perc_entry.get())
     if diff_perc >= 0 and diff_perc <= 100:
       diff_perc_validate.set(gui_texts['good'])
@@ -384,6 +478,7 @@ def validate_config():
     correct_values = False
 
   try:
+    # Coords must be dictionary type
     tl_coord = eval(coords_tl.get())
     br_coord = eval(coords_br.get())
   except:
@@ -393,48 +488,85 @@ def validate_config():
   return correct_values
 
 def clear_config_status():
+  """
+  Clears config validate messages
+  """
   step_validate.set("")
   diff_perc_validate.set("")
   save_file_success.set("")
   coords_validate.set("")
 
 def check_for_coords_thread():
+  """
+  Every 2 seconds checks if coordinates has been set
+
+  :return: 
+    False if coords were set but were incorrect
+  """
   global set_coords_thread_time
+  # Get current window ID
   current_window_id = int(list(windows.keys())[list(windows.values()).index(clicked_window.get())])
+  # Call method to set ss coords
   ss.set_ss_coords(current_window_id)
+  
+  # Loop break means coords has been set
+  # Doesn't matter if they are correct or incorrect
+  # After 30 seconds, stop listening for coords
   while not ss.thread_stop_event.is_set():
     if (time.time() - set_coords_thread_time) > 30:
       break
     time.sleep(2)
   
+  # Set ss coords button back to "clickable"
   config_ss_coords_button['state'] = "normal"
+  # Set save config button "clickable"
   config_save['state'] = "normal"
   
+  # Check if coords has been set correctly
+  # If not, display error message
   if not (ss.top_left_coords and ss.bottom_right_coords):
     coords_validate.set(gui_texts['not_set'])
     return False
 
+  # Get correct coords and display them in GUI
   coord_top_left, coord_bottom_right = ss.top_left_coords, ss.bottom_right_coords
   coords_validate.set(gui_texts['good'])
   coords_tl.set(str(coord_top_left))
   coords_br.set(str(coord_bottom_right))
   
 def set_ss_coords():
+  """
+  Btn call function
+  Starts function checking if coords were set in thread
+  """
   global set_coords_thread_time
   try:
     set_coords = threading.Thread(target=check_for_coords_thread)
     set_coords.setDaemon(True)
     set_coords_thread_time = time.time()
     set_coords.start()
+    # Disable buttons:
+    #   Set ss coords and Save config
+    # while setting coordinates in order not to create multiple identical threads
     config_ss_coords_button['state'] = "disable"
     config_save['state'] = "disable"
   except:
     pass
 
 def change_lang(e):
+  """
+  Calls method to change GUI language
+
+  :param e: lang to be set. e.g "pl", "en"
+  """
   ss.set_language(clicked_lang.get())
 
 def change_theme(t):
+  """
+  Calls method to change GUI theme
+
+  :param t: theme to be set. e.g "dark", "light"
+  """
   ss.set_theme(clicked_theme.get())
 
 # User Set Language
@@ -623,14 +755,29 @@ profile_name = Entry(
 )
 
 def show_profile_name_input(text):
+  """
+  On time while adding new profile,
+  display input getting profile name
+  
+  :param text: profile name text to be inserted into input
+  """
   profile_name.place(relx=0.35, rely=8*0.08)
   profile_name.delete(0, END)
   profile_name.insert(0, text)
 
 def hide_profile_name_input():
+  """
+  Hides profile name input
+  """
   profile_name.place_forget()
 
 def highlight_current_profile():
+  """
+  Changes background of current profile button
+  """
+
+  # Loop throught every profile button and
+  # change bg of current one
   for p in profile_button:
     if p['text'] == ss.config["current_profile"]:
       p.configure(bg=theme['CONFIG_CURRENT_PROF_BG'])
@@ -638,31 +785,53 @@ def highlight_current_profile():
       p.configure(bg=theme['CONFIG_PROF_BG'])
 
 def set_profile(profile):
+  """
+  Sets profile to be current active
+  
+  :param profile: profile to be set to active
+  """
   ss.set_active_profile(profile)
   highlight_current_profile()
   hide_profile_name_input()
+  # Insert new profiles values into inputs
   insert_config_values()
+  # Set Save Button action to "Save"
   config_save.configure(command=lambda action="save": save_config(action))
 
 def new_profile_form():
+  """
+  Prepares config form for creating new profile by
+  - clearing all values
+  - displaying input of new profile's name
+  """
   clear_config_values()
   show_profile_name_input(gui_texts['profile_name'])
+  
+  # Set Save Button action to "Add new profile"
   config_save.configure(command=lambda action="add": save_config(action))
 
+# Buttons of all available profiles
 profile_button = []
 
 def display_profiles():
+  """
+  Updates displayed list of available profiles buttons
+  """
   global profile_button
   
+  # Hide curretly displaying profile buttons
   if profile_button:
     for p in profile_button:
       p.place_forget()
-      
+  
+  # Clear list with available profiles
   profile_button.clear()
 
+  # Get all available profiles
   config_profiles = list(ss.config['profile'].keys())
 
-  i = 0.18
+  i = 0.18 # i = "y" position of button
+  # Loop throught all available profiles and display them
   for j in range(len(config_profiles)):
     profile_button.append(Button(
       config_frame, 
@@ -673,8 +842,10 @@ def display_profiles():
       cursor=theme["CURSOR_ON_HOVER"])
       )
     profile_button[j].place(rely=i, relx=1, anchor=E)
+    # Increase "y" position of every btn
     i += 0.046
 
+  # Add new profile button
   add_profile_button = Button(
     config_frame, 
     text=u"\u2795", 
@@ -684,15 +855,25 @@ def display_profiles():
     cursor=theme["CURSOR_ON_HOVER"]
   )
   add_profile_button.place(rely=0.08, relx=1, anchor=E)
+  # Change bg of currently active profile
   highlight_current_profile()
   
 def rename_profile():
+  """
+  Displays profile name input and 
+  changes "Save profile" btn action to "Rename profile" 
+  """
   show_profile_name_input(ss.config['current_profile'])
   config_save.configure(command=lambda action="rename": save_config(action))
 
 def delete_profile():
+  """
+  Calls deleting profile method
+  """
   if ss.delete_profile(ss.config['current_profile']):
+    # Display available profiles after delete
     display_profiles()
+    # Set first available profile as active
     set_profile((ss.get_profiles())[0])
 
 rename_profile = Button(
@@ -800,6 +981,11 @@ desc_what_coords_lbl = Label(
 
 ############ Info Page ############
 def open_url(url):
+  """
+  Opens url in browser
+
+  :param url: url to be opened
+  """
   webbrowser.open_new(url)
 
 info_author_label = Label(
@@ -823,9 +1009,14 @@ info_author_link_label.bind("<Button-1>", lambda e: open_url("https://github.com
 ###################################
 
 def on_window_close():
+  """
+  Clearing on program exit
+  Stops main program loop and destroys GUI root object
+  """
   exit_program = True
   ss.stop_program()
   root.destroy()
 
+# Before window close, call on_window_close() function
 root.protocol("WM_DELETE_WINDOW", on_window_close)
 root.mainloop()
